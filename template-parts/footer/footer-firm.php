097909b3c9a03d6f05a5f002e65fdc00eeac6e85
SebasTyler <ibikatti@gmail.com>
2021-03-02 10:10:46 -0400

    Cambios en paddings en hero, links & copyright

--- template-parts/footer/footer-firm.php
+++ template-parts/footer/footer-firm.php
@@ -8,6 +8,6 @@
 ?>
 
 <div class="m-footer__firm">
-    <p class="a-footer__copyright"><?php echo '© '. date('Y') . ' Prointelecto'; ?></p>
+    <p class="a-footer__copyright"><?php echo '© '. date('Y') . ' Monlogo, una división de servicios de <a href="https://prointelecto.com"> Prointelecto C.A. </a>'; ?></p>
     <p class="a-footer__dev">Desarrollado por <a href="https://monlogo.co" class="-monlogo" target="_blank">Monlogo</a></p>
 </div>
